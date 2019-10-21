64467a23a12e8d3f239112547eea0f72e29b4aa1
Annisa Rusydina S <56222966+annisanrs30@users.noreply.github.com>
2019-10-21 09:32:58 +0700

    Add files via upload

---
+++ Annisa Function.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void Assalamualaikum() {
+		printf("\n\n Assalamualaikum wr.wb.");
+}
+
+
+
+void perkenalan() {
+	printf("\n\n Hallo !!");
+	printf("\n Perkenalkan Nama Saya Annisa Rusydina Sabila");
+	printf("\n Asal Saya Dari Madura");
+	printf("\n Saya sekarang tinggal di Sidoarjo");
+	printf("\n Saya berkuliah di Universitas Negeri Surabaya");
+	printf("\n Motto Saya : Hidup itu pilihan");
+	printf("\n\n Harapan kedepannya saya ingin memiliki bsnis berbasis IT");
+}
+
+void penutupan() {
+	printf("\n Sekian");
+	printf("\n Wassalamualaikum wr.wb.");
+}
+
+int main () {
+	Assalamualaikum();
+  	perkenalan();
+  	penutupan();
+}
